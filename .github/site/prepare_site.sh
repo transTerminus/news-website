7610c71b3eb1da968b8fb4fd5ec23318cdfa7fcd
Yusheng Zheng <yunwei356@gmail.com>
2024-11-05 08:05:29 +0000

    update

--- .github/site/prepare_site.sh
+++ .github/site/prepare_site.sh
@@ -11,15 +11,10 @@ echo "Files checked and remove large files successfully!"
 mkdir -p docs
 
 # Copy markdown files to docs directory
-cp -r "README.md" "docs/"
-cp -r "健康护理" "docs/"
-cp -r "新闻" "docs/"
-cp -r "法律法规与条款" "docs/"
-cp -r "生活" "docs/"
-cp -r "社群与个人故事" "docs/"
-cp -r "文学与艺术作品" "docs/"
-cp -r "讨论与思考" "docs/"
-cp -r "未知" "docs/"
+# Enable extended globbing if needed
+shopt -s extglob
+cp -r !(docs) docs/
+
 # Copy all files from .github/site to root directory
 cp -r .github/site/* ./
 
